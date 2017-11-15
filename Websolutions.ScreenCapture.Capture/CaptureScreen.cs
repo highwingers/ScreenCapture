fc607b1e017339cf9d6bb6f9be267b54ea1e0426
eliza <eliza@DELL-2IN1>
2017-11-15 14:56:44 -0500

    Add project files.

---
+++ Websolutions.ScreenCapture.Capture/CaptureScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Websolutions.ScreenCapture.Capture
+{
+    public  class CaptureScreen
+    {
+        public static void Capture(string fileName)
+        {
+            using (Bitmap bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                            Screen.PrimaryScreen.Bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bmpScreenCapture))
+                {
+                    g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                     Screen.PrimaryScreen.Bounds.Y,
+                                     0, 0,
+                                     bmpScreenCapture.Size,
+                                     CopyPixelOperation.SourceCopy);
+                }
+
+                bmpScreenCapture.Save(@"Screens\" + fileName + @".png", ImageFormat.Png);
+            }
+        }
+    }
+}
---
+++ Websolutions.ScreenCapture.Client/main.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Websolutions.ScreenCapture.Capture;
+using Websolutions.ScreenCapture.Client.Hotkeys;
+
+namespace Websolutions.ScreenCapture.Client
+{
+    public partial class main : Form
+    {
+
+        private Hotkeys.GlobalHotkey ghk;
+
+        public main()
+        {
+            InitializeComponent();
+            ghk = new Hotkeys.GlobalHotkey(Constants.ALT , Keys.P, this);
+        }
+        // this is ON CLICK off HOTKEY
+        private void HandleHotkey()
+        {
+            WriteLine("Hotkey pressed!");
+            var fileName = Microsoft.VisualBasic.Interaction.InputBox("Enter File Name", "ScreenShot Name", "");
+            System.Threading.Thread.Sleep(500);
+            CaptureScreen.Capture(fileName);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WriteLine("Trying to register SHIFT+ALT+O");
+            if (ghk.Register())
+                WriteLine("Hotkey registered.");
+            else
+                WriteLine("Hotkey failed to register");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!ghk.Unregiser())
+                MessageBox.Show("Hotkey failed to unregister!");
+        }
+
+        private void WriteLine(string text)
+        {
+            textBox1.Text += text + Environment.NewLine;
+        }
+
+    }
+}
